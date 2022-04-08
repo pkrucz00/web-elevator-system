1f35aa620f21125afc2c376ac9b4a85f76cce4f8
pkrucz00 <pawel.kruczkiewicz@gmail.com>
2022-04-08 12:22:16 +0200

    Added openapi

---
+++ src/main/java/pl/kruczkiewicz/pawel/elevator_system/ElevatorSystemApplication.java
@@ -0,0 +1,13 @@
+package pl.kruczkiewicz.pawel.elevator_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElevatorSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElevatorSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/kruczkiewicz/pawel/elevator_system/ElevatorSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kruczkiewicz.pawel.elevator_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElevatorSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
